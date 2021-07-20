75a8d56e41e8d8eedf30c98bddae7864cd07b4e5
lrr68 <lucca@luccaaugusto.xyz>
2021-07-20 20:17:33 -0300

    make script more POSIX compliant

--- punch_clock.sh
+++ punch_clock.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-#-Keeps track of the time i have been working.
-#-Uses the -i sed GNU extension
+#- POSIX compliant shell script to keep track of the time i have been working.
+#- Needs another script that can add and subtract hours (00:00) and convert minutes to HH:MM.
+#- This other script is provided at https://raw.githubusercontent.com/lrr68/punch_the_clock/main/date_time.sh and https://raw.githubusercontent.com/lrr68/rice/master/.local/bin/data_hora
 
 # Script capable of doing operations with date and time
 DATEMATHICS="date_time.sh"
@@ -32,19 +33,21 @@ showtimeworked()
 loglogin()
 {
 	TIME=$(date +%H:%M)
-	TODAY="$(grep -r "^$DATE" $TIMEFILE)"
+	TODAY=$(grep -r "^$DATE" $TIMEFILE)
 
 	if [ ! "$TODAY" ]
 	then
 		echo "$DATE, $TIME, 0," >> $TIMEFILE
 		echo "TRUE" > $HOME/.working
 	else
-		# backup file just in case
-		cp $TIMEFILE "$TIMEFILE.bkp"
+
+		[ -a $HOME/.working ] && echo "Already Logged In" && return
 
 		LOGGEDOUT="$(echo $TODAY | awk '{print $4}')"
 		if [ "$LOGGEDOUT" ]
 		then
+			# backup file just in case
+			cp $TIMEFILE "$TIMEFILE.bkp"
 			echo "$TIME" > $HOME/.working
 		fi
 	fi
@@ -83,7 +86,9 @@ logpause()
 		else
 			NEWLINE=$(echo $TODAY | awk '{print $1" " $2" " '$TOTALDOWNTIME'","}')
 		fi
-		[ "$NEWLINE" ] && sed -i "s/$TODAY/$NEWLINE/g" $TIMEFILE
+		[ "$NEWLINE" ] &&
+			sed "s/$TODAY/$NEWLINE/g" < $TIMEFILE > "$TIMEFILE.aux" &&
+			mv "$TIMEFILE.aux" $TIMEFILE
 	else
 		echo "ERROR: To log a pause inform pause time"
 	fi
@@ -101,24 +106,30 @@ loglogout()
 		if [ ! "$LOGGEDOUT" ]
 		then
 			# compute work hours
-			NEWLINE="$TODAY $TIME, 0, $($DATEMATHICS -m "$TIMEWORKED")"
+			NEWLINE="$TODAY $TIME, 00:00, $($DATEMATHICS -m$TIMEWORKED)"
 		else
 			# compute extra hours
-			LOGIN="$(cat $HOME/.working)"
+			[ -a $HOME/.working ] &&
+			LOGIN="$(cat $HOME/.working)" ||
+			echo "Not Logged in" && return
+
 			EXTRAWORKED="$(echo $TODAY | awk '{print $5}')"
 			EXTRAWORKED=${EXTRAWORKED%,}
+			echo EXTRAWORKED $EXTRAWORKED
 			EXTRA="$($DATEMATHICS -s $TIME $LOGIN)"
 			EXTRA="$($DATEMATHICS -m $EXTRA)"
 			EXTRA="$($DATEMATHICS -a $EXTRA $EXTRAWORKED)"
 
-			TOTALTIME="$(echo $TODAY | awk '{print $6}')"
+			TOTALTIME="$(gettimeworked)"
+			TOTALTIME="$($DATEMATHICS -m $TOTALTIME)"
+			echo totaltime $TOTALTIME
 			TOTALTIME="$($DATEMATHICS -a $EXTRA $TOTALTIME)"
 
 			NEWLINE="$(echo $TODAY | awk '{print $1" " $2" " $3" " $4}') $EXTRA, $TOTALTIME"
-			echo $NEWLINE
 		fi
 
-		sed -i "s/$TODAY/$NEWLINE/g" $TIMEFILE
+		sed "s/$TODAY/$NEWLINE/g" < $TIMEFILE >"$TIMEFILE.aux" &&
+			mv "$TIMEFILE.aux" $TIMEFILE
 		rm $HOME/.working
 	else
 		echo "You must be logged in to logout"
@@ -135,7 +146,7 @@ timetilexit()
 		MSG="You're already working over hours for ${TIMELEFT#-} minutes"
 	else
 		TIMELEFT=$($DATEMATHICS -m $TIMELEFT)
-		MSG="$TIMELEFT left"
+		MSG="$TIMELEFT left, estimated exit: $(data_hora -a $TIME $TIMELEFT)"
 	fi
 
 	case "$1" in
@@ -162,7 +173,7 @@ case "$1" in
 	resume)
 		resumework
 		;;
-	pause)
+	logpause)
 		shift
 		logpause "$1"
 		;;
@@ -179,12 +190,12 @@ case "$1" in
 	*)
 		echo "usage: ${0##*/} ( command )"
 		echo "commands:"
-		echo "			login: Register the login time and sets status as working"
-		echo "			logpause <time in minutes>: Register a pause of X minutes where X is the time informed"
-		echo "			logout: Register the logout time and removes the working status"
-		echo "			break: Register the time you are taking a break"
-		echo "			resume: Remove the break status and register the time you have been out"
-		echo "			left [ notify ]: Informs time left for you to complete 8 hours of work"
-		echo "			timeworked: Informs time you have already worked in this session"
+		echo "		login: Register the login time and sets status as working"
+		echo "		logpause <time in minutes>: Register a pause of X minutes where X is the time informed"
+		echo "		logout: Register the logout time and removes the working status"
+		echo "		break: Register the time you are taking a break"
+		echo "		resume: Remove the break status and register the time you have been out"
+		echo "		left [ notify ]: Informs time left for you to complete 8 hours of work"
+		echo "		timeworked: Informs time you have already worked in this session"
 		;;
 esac
